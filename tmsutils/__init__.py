5846db4fa6f682b6e90d831b8f322c6fa8b7565d
Mrinal Sinha <themrinalsinha@gmail.com>
2019-09-19 01:59:44 +0530

    file split function added

--- tmsutils/__init__.py
+++ tmsutils/__init__.py
@@ -1,3 +1,3 @@
-from .utils import csv_to_linear_tsv
+from .utils import csv_to_linear_tsv, file_split
 
-__all__ = ['csv_to_linear_tsv']
+__all__ = ['csv_to_linear_tsv', 'file_split']
--- tmsutils/utils.py
+++ tmsutils/utils.py
@@ -17,3 +17,22 @@ def csv_to_linear_tsv(csv_path):
         with open(splitext(csv_path)[0] + '.tsv', 'w') as tsvfile:
             for record in reader(csvfile):
                 tsvfile.write(format_fields(record) + '\n')
+
+def file_split(file_path, parts=10):
+    '''
+    Function to split given txt or csv file into given number of parts.
+    '''
+    def chunks(ids, parts):
+        for i in range(0, len(ids), parts):
+            yield ids[i:i+parts]
+
+    with open(file_path, 'r') as f:
+        _file, _ext = splitext(file_path)
+        _id = [x.strip() for x in f.readlines() if x]
+        if not _id:
+            return 'Empty file!'
+        _no = len(_id) // parts
+        val = chunks(_id, _no)
+        for i, v in enumerate(val):
+            with open('{}_{}{}'.format(_file, i, _ext), 'w') as f:
+                f.write('\n'.join(x for x in v))
