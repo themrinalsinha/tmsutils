7f78c27847ddf496b5b1d28b4792010348712a8f
Mrinal Sinha <themrinalsinha@gmail.com>
2019-09-18 09:44:08 +0530

    added gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    url              = 'https://github.com/TheMrinalSinha/tmsutils',
+    name             = 'tmsutils',
+    author           = 'Mrinal Sinha',
+    version          = '0.0.1',
+    packages         = ['tmsutils'],
+    description      = 'Handy tools out of the box',
+    author_email     = 'mail@themrinalsinha.com',
+    install_requires = [pkg for pkg in open('requirements.txt').read().split('\n') if pkg],
+    classifiers      = [
+        "Programming Language :: Python :: 3"
+    ],
+    long_description = open('README.md').read(),
+    long_description_content_type = "text/markdown",
+)
