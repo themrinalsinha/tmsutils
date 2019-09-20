783524fbc299e62386fe78068f1e11563a48e952
Mrinal Sinha <themrinalsinha@gmail.com>
2019-09-20 13:59:01 +0530

    versions updated

--- tmsutils/__init__.py
+++ tmsutils/__init__.py
@@ -1,3 +1,3 @@
-from .utils import csv_to_linear_tsv, file_split
+from .utils import csv_to_linear_tsv, file_split, merge_sqlite_db
 
-__all__ = ['csv_to_linear_tsv', 'file_split']
+__all__ = ['csv_to_linear_tsv', 'file_split', 'merge_sqlite_db']
