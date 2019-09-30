e64f612a645e59bdbf81f60da2941b780c9a16af
Mrinal Sinha <themrinalsinha@gmail.com>
2019-10-01 01:46:20 +0530

    added file.io api to __init__.py

--- tmsutils/__init__.py
+++ tmsutils/__init__.py
@@ -2,7 +2,7 @@ import boto3, botocore
 from .utils      import (csv_to_linear_tsv, file_split, merge_sqlite_db,
                         get_string_combinations, get_string_product)
 from .s3_support import S3
+from .file_io    import FileIO
 
-
-__all__ = ['csv_to_linear_tsv', 'file_split', 'merge_sqlite_db', 'S3',
+__all__ = ['csv_to_linear_tsv', 'file_split', 'merge_sqlite_db', 'S3', 'FileIO',
            'get_string_combinations', 'get_string_product', 'boto3', 'botocore']
