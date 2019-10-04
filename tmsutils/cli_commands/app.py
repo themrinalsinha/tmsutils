f83b8999d5901521adc4008ebace85ee334d097a
Mrinal Sinha <themrinalsinha@gmail.com>
2019-10-05 00:21:20 +0530

    docstring added

--- tmsutils/cli_commands/app.py
+++ tmsutils/cli_commands/app.py
@@ -13,6 +13,18 @@ def cli():
 @click.argument('types', nargs=-1, required=True)
 @click.option('--path', default=getcwd(), help='path where you want to generate .gitignore file')
 def gi(path, types):
+    '''
+    gi - it is a command line utility that helps you to generate .gitignore file for given filetypes
+    USE:
+        $ tms gi python go // or
+        $ tms gi python,go
+
+        but, in case of any such file type which is not supported
+        eg: $ tms gi python3,abcdx
+        you'll get suggestion for each keyword
+        "python3" is invalid, try from "python"
+        "abcdx" is invalid
+    '''
     _types        = ','.join(types).split(',')
     valid_types   = get('https://gitignore.io/api/list').text.split('\n')
     valid_types   = ','.join(valid_types).split(',')
