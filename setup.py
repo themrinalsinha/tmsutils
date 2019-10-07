74ef3064f3f876723dc433a7d0c9a8a7523dadf3
Mrinal Sinha <themrinalsinha@gmail.com>
2019-10-07 12:24:33 +0530

    version updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     url              = 'https://github.com/TheMrinalSinha/tmsutils',
     name             = 'tmsutils',
     author           = 'Mrinal Sinha',
-    version          = '1.0.3',
+    version          = '1.0.4',
     packages         = ['tmsutils'],
     py_modules       = ['tmsutils'],
     description      = 'Handy tools out of the box',
